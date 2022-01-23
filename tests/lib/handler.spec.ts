63904e4c9cba280a153f14d8d645aa030a684e04
DetachHead <detachhead@users.noreply.github.com>
2022-01-23 23:39:34 +1000

    clean up test

--- tests/lib/handler.spec.ts
+++ tests/lib/handler.spec.ts
@@ -4,16 +4,14 @@ import assert from 'typed-nodejs-assert'
 // eslint-disable-next-line @typescript-eslint/no-var-requires -- https://github.com/microsoft/TypeScript/issues/45358
 const items: DownloadPage_FileList_Item[] = require('../fixtures/gumroadItems.json')
 
-describe('getFolderPathsFromId', () => {
-    test('asdf', () => {
-        assert.deepStrictEqual(
-            [
-                'a folder/a file',
-                'folder with nested folder/asdf',
-                'folder with nested folder/nested folder/file within nested folder',
-                'top level file',
-            ],
-            getFolderPaths(items),
-        )
-    })
+test('getFolderPaths', () => {
+    assert.deepStrictEqual(
+        [
+            'a folder/a file',
+            'folder with nested folder/asdf',
+            'folder with nested folder/nested folder/file within nested folder',
+            'top level file',
+        ],
+        getFolderPaths(items),
+    )
 })
