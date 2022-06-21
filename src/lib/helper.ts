8d16e14d1b5f318639a78ee285cbb3c30076cb4e
DetachHead <detachhead@users.noreply.github.com>
2022-06-21 18:42:36 +1000

    update selector for file list json

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -1,11 +1,15 @@
 import { AxiosResponse } from 'axios'
 import { load } from 'cheerio'
 import { throwIfUndefined } from 'throw-expression'
+import { Element } from 'domhandler'
 
-export const getAttribute = (response: AxiosResponse, selector: string, attr: string): string => {
-    const $ = load(response.data)
-    return throwIfUndefined(
-        throwIfUndefined($(selector)[0], `failed to find selector: ${selector}`).attribs[attr],
+export const getAttribute = (response: AxiosResponse, selector: string, attr: string): string =>
+    throwIfUndefined(
+        getSelector(response, selector).attribs[attr],
         `failed to find "${attr}" attr on $("${selector}")`,
     )
+
+export const getSelector = (response: AxiosResponse, selector: string): Element => {
+    const $ = load(response.data)
+    return throwIfUndefined($(selector)[0], `failed to find selector: ${selector}`)
 }
